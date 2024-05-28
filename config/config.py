9533d83f5a6ab8cb372c4ea89f762ce02c8c3867
profe benja <bej.mora@profesor.duoc.cl>
2024-05-28 01:52:17 -0400

    icon compilation error

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
 
 DB_SERVICE_NAMES = ["orcl", "xe"]
-# ICON_PATH = os.path.join(os.path.dirname(__file__), '..', 'icono.ico')
+ICON_PATH = os.path.join(os.path.dirname(__file__), '..', 'icono.ico')
 YOUTUBE_CHANNEL_URL = "https://www.youtube.com/@code0code/videos"
